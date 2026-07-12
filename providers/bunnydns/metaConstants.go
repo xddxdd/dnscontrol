b425203f57245fca53c587b3f0a9e53ff7a986a4
Lan Tian <xuyh0120@outlook.com>
2026-07-12 13:14:20 -0700

    BUNNYDNS: add GeoDNS metadata support

---
+++ providers/bunnydns/metaConstants.go
@@ -0,0 +1,8 @@
+package bunnydns
+
+const (
+	metaSmartRoutingType     = "bunny_smart_routing_type"
+	metaGeolocationLatitude  = "bunny_geolocation_latitude"
+	metaGeolocationLongitude = "bunny_geolocation_longitude"
+	metaLatencyZone          = "bunny_latency_zone"
+)
--- providers/bunnydns/records.go
+++ providers/bunnydns/records.go
@@ -1,6 +1,7 @@
 package bunnydns
 
 import (
+	"encoding/json"
 	"fmt"
 	"slices"
 
@@ -69,7 +70,7 @@ func (b *bunnydnsProvider) GetZoneRecordsCorrections(dc *models.DomainConfig, ex
 		return nil, 0, err
 	}
 
-	instructions, actualChangeCount, err := diff2.ByRecord(existing, dc, nil)
+	instructions, actualChangeCount, err := diff2.ByRecord(existing, dc, comparableFunc)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -144,4 +145,18 @@ func (b *bunnydnsProvider) mkDeleteCorrection(zoneID int64, oldRec *models.Recor
 			return b.deleteRecord(zoneID, existingID)
 		},
 	}
+}
+
+func comparableFunc(rec *models.RecordConfig) string {
+	if len(rec.Metadata) == 0 {
+		return ""
+	}
+
+	result, err := json.Marshal(rec.Metadata)
+	if err != nil {
+		printer.Warnf("BUNNY_DNS: Cannot serialize metadata of record %s", rec)
+		return ""
+	}
+
+	return string(result)
 }
